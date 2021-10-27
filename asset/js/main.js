ec5b9b3ea1640c4e706d8430001acd8b16c87130
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-10-27 14:30:25 +0200

    TImer working

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,13 @@
 const randomNumbers = generateFiveNumbers();
 
+let timer = 5;
+
+alert("Remember these numbers: " + randomNumbers);
+
+const clock = setInterval(timerOnScreen, 1000);
+
+
+
 function generateFiveNumbers() {
     let numbersToGenerate = []
     for (let i = 0; i < 5; i++) {
@@ -8,4 +16,15 @@ function generateFiveNumbers() {
     return numbersToGenerate;
 }
 
-console.log(randomNumbers);
+function timerOnScreen() {
+    if (timer > 0) {
+        document.querySelector("h1").innerHTML = ` Prepare to repeat them in ${timer} seconds!`
+        timer--;
+    } else {
+        document.querySelector("h1").innerHTML = ` What numbers i showed you?`
+        clock = clearInterval;
+        console.log(timer);
+    }
+
+}
+
