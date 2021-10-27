182acffbbba11959da30e429cc68bbcdc6c51044
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-10-27 15:41:53 +0200

    Finished

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,9 +10,11 @@ const clock = setInterval(timerOnScreen, 1000);
 
 
 function generateFiveNumbers() {
-    let numbersToGenerate = []
+
+    const numbersToGenerate = []
+
     for (let i = 0; i < 5; i++) {
-        numbersToGenerate.push(Math.floor(Math.random() * 100 + 1));
+        numbersToGenerate.push((Math.floor(Math.random() * 100 + 1)));
     }
     return numbersToGenerate;
 }
@@ -30,24 +32,30 @@ function askForNumbers() {
     alert("Ok, now, type here the five numbers i gave you, then i will tell you if you won or not.")
     const numberGuessed = [];
     for (let i = 0; i < 5; i++) {
-        numberGuessed.push(prompt(`Type here the numbers [${i + 1}/5]`));
+
+        numberGuessed.push(parseInt(prompt(`Type here the numbers [${i + 1}/5]`)));
     }
     compareNumbers(randomNumbers, numberGuessed);
 }
 
 function compareNumbers(cpu, user) {
-    let counter;
+    let counter = 0
+        ;
 
     for (let i = 0; i < cpu.length; i++) {
         for (let j = 0; j < user.length; j++) {
             if (cpu[i] == user[j]) {
+                console.log(cpu[i], user[j])
                 counter++;
             }
         }
     }
+    console.log(counter)
 
     if (counter == 5) {
         document.querySelector("h1").innerHTML = `Yeah! You win! Grats~`
+    } else {
+        document.querySelector("h1").innerHTML = `You lose!`
 
     }
 
